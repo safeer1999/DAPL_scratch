35de3313ef5d2a4077ff9057fa80400eda5c0d67
safeer1999 <safeerahmed.v@gmail.com>
2019-06-28 21:59:03 +0300

    fixed minor bug in argument passing

--- predict_missing.py
+++ predict_missing.py
@@ -11,16 +11,16 @@ from DataHandler import DataHandler
 #--------------------------------------Command Line Argument------------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file")
-parser.add_argument("--set_mask", type = bool, default = False)
+#parser.add_argument("--set_mask", type = bool, default = False)
 #parser.add_argument("--lr", type = float, default = 0.001)
 #parser.add_argument("--epochs", type = int, default = 10)
 #parser.add_argument("--batch_size", type = int , default = 100)
 #parser.add_argument("--shape" , default  = '0,0' )
 #parser.add_argument("--missing_perc", type = float , default = 1)
 parser.add_argument("--save_results", type = bool, default = False)
-parser.add_argument("--output_filePath")
+parser.add_argument("--output_filePath", default = 'predicted_results')
 #parser.add_argument("--save_model", type = bool, default = False)
-parser.add_argument("--model_dir","trained_model")
+parser.add_argument("--model_dir", default = "trained_model")
 
 args = parser.parse_args()
 
@@ -30,7 +30,7 @@ args = parser.parse_args()
 
 
 def main() :
-	Dataset = DataHandler(args.input_file, mask_given = args.set_mask)
+	Dataset = DataHandler(args.input_file)
 
 	model = DAPL(shape = Dataset.R.shape)
 	model.restore_graph(args.model_dir)
@@ -48,7 +48,7 @@ def main() :
 
 	X_masked = X[mask == 0]
 	recons_masked = recons_X[mask == 0]
-	acc = Dataset.correl(X_masked, recons_masked)
+	
 
 	if args.save_results :
 		pd.DataFrame(recons_X).to_csv(args.output_filePath + '/recons.csv')
